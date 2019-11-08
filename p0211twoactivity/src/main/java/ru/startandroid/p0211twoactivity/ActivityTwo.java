62fa8c1644f447da052f9bef4af10ecde1108d64
Dmitriy33g <dmitriy33g@gmail.com>
2019-11-09 00:30:18 +0300

    3

--- p0211twoactivity/src/main/java/ru/startandroid/p0211twoactivity/ActivityTwo.java
+++ p0211twoactivity/src/main/java/ru/startandroid/p0211twoactivity/ActivityTwo.java
@@ -2,13 +2,14 @@ package ru.startandroid.p0211twoactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class ActivityTwo extends AppCompatActivity implements View.OnClickListener {
+public class ActivityTwo extends Activity implements View.OnClickListener {
 
     Button btnActMain;
 
--- p0211twoactivity/src/main/java/ru/startandroid/p0211twoactivity/MainActivity.java
+++ p0211twoactivity/src/main/java/ru/startandroid/p0211twoactivity/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Log.d(TAG, "MainActivity: onCreate()");
+        Log.d(TAG, "MainActivity: onCreate() - вызывается при первом создании Activity");
 
         btnActTwo = (Button) findViewById(R.id.btnActTwo);
         btnActTwo.setOnClickListener(this);
@@ -28,31 +28,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "MainActivity: onStart()");
+        Log.d(TAG, "MainActivity: onStart() - вызывается перед тем, как Activity будет видно пользователю");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "MainActivity: onResume()");
+        Log.d(TAG, "MainActivity: onResume() - вызывается перед тем как будет доступно для активности пользователя (взаимодействие)");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "MainActivity: onPause()");
+        Log.d(TAG, "MainActivity: onPause() - вызывается перед тем, как будет показано другое Activity");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "MainActivity: onStop()");
+        Log.d(TAG, "MainActivity: onStop() - вызывается когда Activity становится не видно пользователю");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "MainActivity: onDestroy()");
+        Log.d(TAG, "MainActivity: onDestroy() - вызывается перед тем, как Activity будет уничтожено");
     }
 
     @Override
