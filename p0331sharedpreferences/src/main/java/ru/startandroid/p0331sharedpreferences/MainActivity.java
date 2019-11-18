5720df21d8a5cb7495cfe80218f279fca5e98c9c
Dmitriy33g <dmitriy33g@gmail.com>
2019-11-18 04:44:43 +0300

    9

--- p0331sharedpreferences/src/main/java/ru/startandroid/p0331sharedpreferences/MainActivity.java
+++ p0331sharedpreferences/src/main/java/ru/startandroid/p0331sharedpreferences/MainActivity.java
@@ -1,10 +1,10 @@
 package ru.startandroid.p0331sharedpreferences;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.database.Cursor;
@@ -26,14 +26,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     final String SAVED_TEXT = "saved_text";
 
-    final String LOG_TAG = "myLogs";
+    static final String LOG_TAG = "myLogs";
 
-    Button btnAdd, btnRead, btnClear, btnUpd, btnDel;
-    EditText etName, etEmail, etID;
+    Button btnAdd, btnRead, btnClear, btnUpd, btnDel, btnSQL;
+
+    EditText etName, etPeople, etRegion, etID;
+
+    // данные для таблицы должностей
+    int[] position_id = { 1, 2, 3, 4 };
+    String[] position_name = { "Директор", "Программер", "Бухгалтер", "Охранник" };
+    int[] position_salary = { 15000, 13000, 10000, 8000 };
+
+    // данные для таблицы людей
+    String[] people_name = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис", "Костя", "Игорь" };
+    int[] people_posid = { 2, 3, 2, 2, 3, 1, 2, 4 };
 
     DBHelper dbHelper;
+    SQLiteDatabase db;
 
-    class DBHelper extends SQLiteOpenHelper {
+    static class DBHelper extends SQLiteOpenHelper {
 
         public DBHelper(Context context) {
             super(context, "myDB", null, 1);
@@ -42,11 +53,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         public void onCreate(SQLiteDatabase db) {
             Log.d(LOG_TAG, "--- onCreate database ---");
-            // создаем таблицу с полями
+            // создаем таблицу с полями справочника Стран
             db.execSQL("create table mytable ("
                     + "id integer primary key autoincrement,"
                     + "name text,"
-                    + "email text" + ");");
+                    + "people integer,"
+                    + "region text" + ");");
+
+            // создаем таблицу должностей
+            db.execSQL("create table position ("
+                    + "id integer primary key,"
+                    + "name text,"
+                    + "salary integer" + ");");
         }
 
         @Override
@@ -85,11 +103,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnDel = findViewById(R.id.btnDel);
         btnDel.setOnClickListener(this);
 
+        btnSQL = findViewById(R.id.btnSQL);
+        btnSQL.setOnClickListener(this);
+
         etName = findViewById(R.id.etName);
-        etEmail = findViewById(R.id.etEmail);
+        etPeople = findViewById(R.id.etPeople);
+        etRegion = findViewById(R.id.etRegion);
         etID = findViewById(R.id.etID);
 
         dbHelper = new DBHelper(this);
+        // подключаемся к базе
+        db = dbHelper.getWritableDatabase();
     }
 
     @Override
@@ -107,16 +131,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+        // подключаемся к БД
+        db = dbHelper.getWritableDatabase();
         // создаем объект для данных
         ContentValues cv = new ContentValues();
 
         // получаем данные из полей ввода
         String name = etName.getText().toString();
-        String email = etEmail.getText().toString();
+        String people = etPeople.getText().toString();
+        String region = etRegion.getText().toString();
         String id = etID.getText().toString();
-
-        // подключаемся к БД
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         switch (v.getId()) {
             case R.id.btnSave:
@@ -128,9 +152,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnAdd:
                 Log.d(LOG_TAG, "--- Insert in mytable: ---");
                 // подготовим данные для вставки в виде пар: наименование столбца - значение
-
                 cv.put("name", name);
-                cv.put("email", email);
+                cv.put("people", people);
+                cv.put("region", region);
                 // вставляем запись и получаем ее ID
                 long rowID = db.insert("mytable", null, cv);
                 Log.d(LOG_TAG, "row inserted, ID = " + rowID);
@@ -146,14 +170,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     // определяем номера столбцов по имени в выборке
                     int idColIndex = c.getColumnIndex("id");
                     int nameColIndex = c.getColumnIndex("name");
-                    int emailColIndex = c.getColumnIndex("email");
+                    int peopleColIndex = c.getColumnIndex("people");
+                    int regionColIndex = c.getColumnIndex("region");
 
                     do {
                         // получаем значения по номерам столбцов и пишем все в лог
                         Log.d(LOG_TAG,
                                 "ID = " + c.getInt(idColIndex) +
                                 ", name = " + c.getString(nameColIndex) +
-                                ", email = " + c.getString(emailColIndex));
+                                ", people = " + c.getString(peopleColIndex) +
+                                ", region = " + c.getString(regionColIndex));
                         // переход на следующую строку
                         // а если следующей нет (текущая - последняя), то false - выходим из цикла
                     } while (c.moveToNext());
@@ -174,7 +200,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.d(LOG_TAG, "--- Update mytable: ---");
                 // подготовим значения для обновления
                 cv.put("name", name);
-                cv.put("email", email);
+                cv.put("people", people);
+                cv.put("region", region);
                 // обновляем по id
                 int updCount = db.update(
                         "mytable", cv, "id = ?", new String[] { id });
@@ -189,9 +216,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 int delCount = db.delete("mytable", "id = " + id, null);
                 Log.d(LOG_TAG, "deleted rows count = " + delCount);
                 break;
-            default:
+            case  R.id.btnSQL:
+                Intent intent = new Intent(this, SQLiteQueryActivity.class);
+                startActivity(intent);
                 break;
         }
+
         // закрываем подключение к БД
         dbHelper.close();
     }
